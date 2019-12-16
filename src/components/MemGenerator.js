0ebb38c287cdccd51e7503d0bec52daf2529a523
DmitriySydorenko <dmitry.vitalievich@gmail.com>
2019-12-16 15:43:07 +0200

    memegenerator.js API created (compDidMount) + handleChange function & form block basic

--- src/components/MemGenerator.js
+++ src/components/MemGenerator.js
@@ -1,18 +1,56 @@
 import React from "react"
 
 class MemGenerator extends React.Component {
-    constructor(){
-        super();
-        this.state = {
-            
-        }
-    }
-    render(){
-        return(
-            <div>
-            This is MemGenerator
-            </div>
-        )
-    }
+  constructor() {
+    super();
+    this.state = {
+      topText: "",
+      bottomText: "",
+      randomImg: "/images/mem_1.jpg",
+      allMemeImgs: []
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentDidMount() {
+    fetch("https://api.imgflip.com/get_memes")
+      .then(response => response.json())
+      .then(response => {
+        let { memes } = response.data;
+        console.log(memes);
+        this.setState({ allMemesImg: memes });
+      });
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
+
+  render() {
+    return (
+      <div>
+        <form className="meme-form">
+          <input
+            type="text"
+            name="topText"
+            placeholder="Top Text"
+            value={this.state.topText}
+            onChange={this.handleChange}
+          />
+          <input
+            type="text"
+            name="bottomText"
+            placeholder="Bottom Text"
+            value={this.state.bottomText}
+            onChange={this.handleChange}
+          />
+
+          <button>Generate meme</button>
+        </form>
+      </div>
+    );
+  }
 }
 export default MemGenerator
